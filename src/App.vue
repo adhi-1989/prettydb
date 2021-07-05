439a31dc6fa778d1b0ecf5dbe4b1271eddb762d0
adhi_1989 <account@adhi.jp>
2021-07-05 20:39:46 +0900

    :lipstick: アプリ画面のスタイル構成を見直し

--- src/App.vue
+++ src/App.vue
@@ -115,30 +115,34 @@ html {
   @apply h-full;
 }
 body {
-  @apply h-full font-sans antialiased text-[color:var(--app-text-color-primary-default)] bg-[color:var(--app-bg-color-primary-default)];
+  @apply h-full overflow-hidden font-sans antialiased text-[color:var(--app-text-color-primary-default)] bg-[color:var(--app-bg-color-primary-default)];
 }
 #app {
   @apply h-full;
 }
 #modal-root {
-  @apply absolute z-50;
+  @apply absolute z-[50];
 }
 #modal-view {
-  @apply absolute z-10;
+  @apply absolute z-[10];
 }
 .application-root {
   @apply h-full overflow-hidden;
   > .header {
-    @apply fixed flex justify-between w-full h-[2.5rem] shadow z-40;
+    @apply fixed flex justify-between w-full h-[2.5rem] shadow z-[40];
+    @apply xs:h-[3rem];
     user-select: none;
     > .logo {
       @apply flex items-center w-[50%] px-[0.5rem];
+      @apply xs:w-[16rem] xs:px-[1rem];
       > .router-link > .image {
         @apply w-full;
       }
     }
     > .toggle-menu {
       @apply w-[2.5rem] mr-[0.5rem] cursor-pointer;
+      @apply xs:w-[3rem] xs:mr-[1rem];
+      @apply lg:hidden;
       > .icon {
         &-enter-active {
           animation: fadeIn;
@@ -153,77 +157,37 @@ body {
     }
   }
   > .menu {
-    @apply fixed top-[2.5rem] left-0 flex flex-col w-[50%] h-[calc(100%-2.5rem)] shadow z-30;
+    @apply fixed top-[2.5rem] left-0 flex flex-col w-[50%] h-[calc(100%-2.5rem)] shadow z-[30];
+    @apply xs:top-[3rem] xs:w-[16rem] xs:h-[calc(100%-3rem)];
     background-color: var(--app-bg-color-primary-default);
     transition: left 0.5s ease;
     user-select: none;
     &[data-active="false"] {
       @apply left-[-50%];
+      @apply xs:left-[-16rem];
+      @apply lg:left-0;
     }
     > .menu-item-group {
       @apply flex-grow ml-[10%];
+      @apply xs:ml-[2rem];
       > .menu-item {
         @apply mt-[1rem];
       }
     }
     > .tools {
       @apply flex justify-between items-center h-[2.5rem] px-[0.5rem];
+      @apply xs:h-[3.5rem] xs:py-[0.5rem];
       > .icon {
         @apply w-[2rem] cursor-pointer;
+        @apply xs:w-[2.5rem];
       }
     }
   }
   > .main {
     @apply overflow-auto w-full h-[calc(100%-2.5rem)] mt-[2.5rem];
-  }
-}
-@screen xs {
-  .application-root {
-    > .header {
-      @apply h-[3rem];
-      > .logo {
-        @apply w-[16rem] px-[1rem];
-      }
-      > .toggle-menu {
-        @apply w-[3rem] mr-[1rem];
-      }
-    }
-    > .menu {
-      @apply top-[3rem] w-[16rem] h-[calc(100%-3rem)];
-      &[data-active="false"] {
-        @apply left-[-16rem];
-      }
-      > .menu-item-group {
-        @apply ml-[2rem];
-      }
-      > .tools {
-        @apply h-[3.5rem] py-[0.5rem];
-        > .icon {
-          @apply w-[2.5rem];
-        }
-      }
-    }
-    > .main {
-      @apply h-[calc(100%-3rem)] mt-[3rem];
-      transition: width 0.5s ease, margin-left 0.5s ease;
-    }
-  }
-}
-@screen lg {
-  .application-root {
-    > .header {
-      > .toggle-menu {
-        @apply hidden;
-      }
-    }
-    > .menu {
-      &[data-active="false"] {
-        @apply left-0;
-      }
-    }
-    > .main {
-      @apply w-[calc(100%-16rem)] ml-[16rem];
-    }
+    @apply xs:h-[calc(100%-3rem)] xs:mt-[3rem];
+    @apply lg:w-[calc(100%-16rem)] lg:ml-[16rem];
+    transition: width 0.5s ease, margin-left 0.5s ease;
   }
 }
 </style>
