46eed6cdb1620be45c66423e1cac1943c0d9e5f2
adhi_1989 <account@adhi.jp>
2021-08-27 23:47:50 +0900

    :heavy_minus_sign: animate.css を依存関係から削除

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,6 @@ import App from "./App.vue";
 
 import "virtual:windi.css";
 import "./assets/styles/main.scss";
-// TODO: 必要なアニメーションのみ作成してバンドルサイズを削減する(あるいは gsap に移行する)
-// インストールしたのをそのまま使うとエラーがでるので、問題箇所を削除したものをローカルからロードする
-// https://github.com/animate-css/animate.css/issues/641
-import "./assets/styles/animate.css";
 
 const routes = setupLayouts(generatedRoutes);
 
