9510303144f915d3608950c396fe3ece01acd493
adhi_1989 <account@adhi.jp>
2021-09-20 17:36:13 +0900

    :wrench: 不要なパスエイリアスを削除

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig(() => {
   return {
     resolve: {
       alias: {
-        "~/": `${path.resolve(__dirname, "node_modules")}/`,
         "@/": `${path.resolve(__dirname, "src")}/`,
         "#/assets/": `${path.resolve(__dirname, "src/assets")}/`,
         "#/images/": `${path.resolve(__dirname, "src/assets/images")}/`,
